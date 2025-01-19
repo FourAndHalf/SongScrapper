1c678441c0062241ec71285431891d099bde845d
FourAndHalf <jinsoneb@gmail.com>
2025-01-19 13:57:49 +0530

    Changed folder structure, to see if it can be run

--- SpotifyDownloader/urls.py
+++ SpotifyDownloader/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from django.shortcuts import render
 
+def home_view(request):
+    return render(request, 'link.html')
+    
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('linksaver/', include('LinkSaver.urls')),
+    path('', home_view, name='home')
 ]

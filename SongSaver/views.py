f5baf7e6958ff94329ab8d495584377574805a7f
FourAndHalf <jinsoneb@gmail.com>
2025-01-22 09:41:33 +0530

    Started song searching from spotify using spotipy

--- SongSaver/views.py
+++ SongSaver/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+from spotipy import Spotify
+from spotipy.oauth2 import SpotifyClientCredentials
+import re
 
 def startup_page(request):
     return render(request, 'SongSaver/link.html')
